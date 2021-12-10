f1542a5d3459c8e7432b2d78dbfff8d0a2097db9
midwestE <midwestE@devbunker.com>
2021-12-10 14:36:59 -0600

    Turn unharden into hook

--- contrib/hardening.php
+++ contrib/hardening.php
@@ -86,18 +86,9 @@ task('deploy:harden', function () {
 })->desc('Hardens site permissions');
 
 task('deploy:unharden', function () {
-    unharden('{{release_path}}');
-})->desc('Hardens site permissions');
-
-/* ----------------- Task Overrides ----------------- */
-// TODO - Change these to use before hook
-
-// Needs to be overridden to account for hardened file/directory permissions
-task('deploy:cleanup', function () {
     $releases = get('releases_list');
     $keep = get('keep_releases');
-    $sudo = get('cleanup_use_sudo') ? 'sudo' : '';
-    $runOpts = [];
+    // $sudo = get('unharden_use_sudo') ? 'sudo' : '';
 
     if ($keep === -1) {
         // Keep unlimited releases.
@@ -112,43 +103,7 @@ task('deploy:cleanup', function () {
     foreach ($releases as $release) {
         // Unharden permissions before removal
         unharden("{{deploy_path}}/releases/$release");
-
-        run("$sudo rm -rf {{deploy_path}}/releases/$release", $runOpts);
     }
+})->desc('Unhardens old releases before deploy:cleanup to allow removal of read only files/dirs');
 
-    run("cd {{deploy_path}} && if [ -e release ]; then rm release; fi", $runOpts);
-})->desc('Cleanups old releases after unhardening files');
-
-task('rollback', function () {
-    cd('{{deploy_path}}');
-
-    $currentRelease = basename(run('readlink {{current_path}}'));
-    $candidate = get('rollback_candidate');
-
-    writeln("Current release is <fg=red>$currentRelease</fg=red>.");
-
-    if (!test("[ -d releases/$candidate ]")) {
-        throw new \RuntimeException(parse("Release \"$candidate\" not found in \"{{deploy_path}}/releases\"."));
-    }
-    if (test("[ -f releases/$candidate/BAD_RELEASE ]")) {
-        writeln("Candidate <fg=yellow>$candidate</> marked as <error>bad release</error>.");
-        if (!askConfirmation("Continue rollback to $candidate?")) {
-            writeln('Rollback aborted.');
-            return;
-        }
-    }
-    writeln("Rolling back to <info>$candidate</info> release.");
-
-    // Unharden permissions before removal
-    $server = new ServerHardening();
-    $server->unharden("{{deploy_path}}/releases/$currentRelease");
-
-    // Symlink to old release.
-    run("{{bin/symlink}} releases/$candidate {{current_path}}");
-
-    // Mark release as bad.
-    $timestamp = timestamp();
-    run("echo '$timestamp,{{user}}' > releases/$currentRelease/BAD_RELEASE");
-
-    writeln("<info>rollback</info> to release <info>$candidate</info> was <success>successful</success>");
-})->desc('Rollbacks to the previous release');
+before('deploy:cleanup', 'deploy:unharden');
